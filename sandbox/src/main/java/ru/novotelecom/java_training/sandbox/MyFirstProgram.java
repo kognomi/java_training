e2066f7a98ca06666648f32b560511a56710cfa7
Зенкова Ирина <i.a.zenkova@novotelecom.ru>
2018-02-02 16:57:27 +0700

    добавлено несколько функций

--- sandbox/src/main/java/ru/novotelecom/java_training/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/novotelecom/java_training/sandbox/MyFirstProgram.java
@@ -4,6 +4,23 @@ public class MyFirstProgram {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello, world!");
+		hello("world");
+        hello("vasya");
+        double vasek =5;
+        double a=4;
+        double b=6;
+        System.out.println("Площадь квадрата со стороной " + vasek + " = " + area (vasek));
+        System.out.println("Площадь квадрата со сторонами " + a +" и " + b + " = " + area(a,b));
 	}
+
+	public static void hello(String somebody) {
+
+	   System.out.println("Hello, " + somebody+ "!");
+    }
+    public static double area (double kot) {
+	    return kot*kot;
+    }
+    public static double area (double a,double b) {
+	    return a*b;
+    }
 }
