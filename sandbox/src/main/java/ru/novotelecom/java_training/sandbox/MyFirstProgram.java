cfa51e5daba983ce8bab2afe29a3cb99ce08b3b5
Зенкова Ирина <i.a.zenkova@novotelecom.ru>
2018-02-05 10:42:13 +0700

    добавлены функция и метод вычисления расстояния между двумя точками

--- sandbox/src/main/java/ru/novotelecom/java_training/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/novotelecom/java_training/sandbox/MyFirstProgram.java
@@ -12,6 +12,14 @@ public class MyFirstProgram {
 
         Rectangle rectangle= new Rectangle(4,6);
         System.out.println("Площадь квадрата со сторонами " + rectangle.a +" и " + rectangle.b + " = " + rectangle.area());
+
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(2,2);
+        System.out.println("Расстояние между точками (" +p1.x + "," + p1.y + ")"  +" и " +  "(" +p2.x + "," + p2.y + ")"+ " = " + distance(p1,p2));
+        // есть подозрения, что написанное тут - жуть жутчайшая, но как по-другому я без понятия, и вроде оно работает
+        System.out.println("Расстояние между точками (" +p1.x + "," + p1.y + ")"  +" и " +  "(" +p2.x + "," + p2.y + ")"+ " = " + p2.distance(p1,p2));
+
+
 	}
 
 	public static void hello(String somebody) {
@@ -19,11 +27,10 @@ public class MyFirstProgram {
 	   System.out.println("Hello, " + somebody+ "!");
     }
 
-/*   public static double distance(Point p1, Point p2) {
+ public static double distance(Point p1, Point p2) {
 
-
-return;
-   }*/
+	    return Math.sqrt((Math.pow((p2.x-p1.x),2) + Math.pow((p2.y-p1.y),2)));
+   }
 
 
 }
--- sandbox/src/main/java/ru/novotelecom/java_training/sandbox/Point.java
+++ sandbox/src/main/java/ru/novotelecom/java_training/sandbox/Point.java
@@ -10,4 +10,9 @@ public class Point {
         this.x=x;
         this.y=y;
     }
+    public double distance (Point p1, Point p2) {
+        // есть подозрения, что написанное тут - жуть жутчайшая, но как по-другому я без понятия, и вроде оно работает
+
+        return Math.sqrt((Math.pow((p2.x-p1.x),2) + Math.pow((p2.y-p1.y),2)));
+    }
 }
