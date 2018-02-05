04743b7cc7eff9e8420129b9710a1dc565ca6e9f
Зенкова Ирина <i.a.zenkova@novotelecom.ru>
2018-02-05 10:13:18 +0700

    добавлены классы прямоугольника, квадрата и точек, методы вычисления площадей

--- sandbox/src/main/java/ru/novotelecom/java_training/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/novotelecom/java_training/sandbox/MyFirstProgram.java
@@ -6,21 +6,24 @@ public class MyFirstProgram {
 
 		hello("world");
         hello("vasya");
-        double vasek =5;
-        double a=4;
-        double b=6;
-        System.out.println("Площадь квадрата со стороной " + vasek + " = " + area (vasek));
-        System.out.println("Площадь квадрата со сторонами " + a +" и " + b + " = " + area(a,b));
+
+        Square square= new Square(5);
+        System.out.println("Площадь квадрата со стороной " + square.l + " = " + square.area());
+
+        Rectangle rectangle= new Rectangle(4,6);
+        System.out.println("Площадь квадрата со сторонами " + rectangle.a +" и " + rectangle.b + " = " + rectangle.area());
 	}
 
 	public static void hello(String somebody) {
 
 	   System.out.println("Hello, " + somebody+ "!");
     }
-    public static double area (double kot) {
-	    return kot*kot;
-    }
-    public static double area (double a,double b) {
-	    return a*b;
-    }
+
+/*   public static double distance(Point p1, Point p2) {
+
+
+return;
+   }*/
+
+
 }
---
+++ sandbox/src/main/java/ru/novotelecom/java_training/sandbox/Point.java
@@ -0,0 +1,13 @@
+package ru.novotelecom.java_training.sandbox;
+
+public class Point {
+
+    public double x;
+    public double y;
+
+    public Point (double x,double y) {
+
+        this.x=x;
+        this.y=y;
+    }
+}
