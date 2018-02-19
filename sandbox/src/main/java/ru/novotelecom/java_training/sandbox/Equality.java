85f6672e1347d65b483b4544e832eeb2ba7417f0
Зенкова Ирина <i.a.zenkova@novotelecom.ru>
2018-02-19 14:39:53 +0700

    изучение сравнения

--- sandbox/src/main/java/ru/novotelecom/java_training/sandbox/Equality.java
+++ sandbox/src/main/java/ru/novotelecom/java_training/sandbox/Equality.java
@@ -2,12 +2,12 @@ package ru.novotelecom.java_training.sandbox;
 
 public class Equality {
     public static void main(String[] args ) {
-        //String s1 = "firefox";
-        String s1 = "firefox 2.0";
+        String s1 = "firefox";
+        //String s1 = "firefox 2.0";
         //String s2 = new String(s1);
         //String s2 = "firefox";
-        //String s2 = "fir" +"efox";
-        String s2 = "firefox " + Math.sqrt(4.0);
+        String s2 = "fir" +"efox";
+        //String s2 = "firefox " + Math.sqrt(4.0);
 
 
         System.out.println(s1 == s2);
