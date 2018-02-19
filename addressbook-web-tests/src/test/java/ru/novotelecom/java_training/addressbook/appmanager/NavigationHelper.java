34889d152c3a305099bc2f97aec0afb52da759cc
Зенкова Ирина <i.a.zenkova@novotelecom.ru>
2018-02-19 17:09:30 +0700

    оптимизированны переходы между страницами 2

--- addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/appmanager/NavigationHelper.java
@@ -28,6 +28,12 @@ public class NavigationHelper extends HelperBase {
 
 
     public void initContactCreation() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")
+            && isElementPresent(By.name("submit")) && wd.findElement(By.name("submit")).getText().equals("Enter")) {
+            return;
+        }
+
         click(By.linkText("add new"));
     }
 }
