69df0e616642e39f29b5eff41f80e50193c5762d
Зенкова Ирина <i.a.zenkova@novotelecom.ru>
2018-02-26 09:18:35 +0700

    Сравнение списков при удалении группы

--- addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/tests/GroupDeletionTests.java
@@ -11,15 +11,18 @@ public class GroupDeletionTests extends TestBase {
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().goToGroupPage();
-        if (! app.getGroupHelper().isThereAGroup()) {
-            app.getGroupHelper().createGroup(new GroupData("test1",null,null));
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(before.size()-1);
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
-        List <GroupData> after = app.getGroupHelper().getGroupList();
-        Assert.assertEquals(after.size(),before.size()-1);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
     }
 
 
