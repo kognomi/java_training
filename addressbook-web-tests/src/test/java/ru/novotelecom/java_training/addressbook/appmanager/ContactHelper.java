929f026fb80ffd6b00c1b9f770f251fb030edd09
Зенкова Ирина <i.a.zenkova@novotelecom.ru>
2018-02-27 08:56:39 +0700

    тест создания контакта со сравнением полученных списков

--- addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/appmanager/ContactHelper.java
@@ -88,10 +88,10 @@ public class ContactHelper  extends HelperBase{
     public List<ContactData> getContactList() {
         List <ContactData> contacts = new ArrayList<ContactData>();
        // List<WebElement> elements = wd.findElement(By.id("maintable")).findElements(By.className("entry"));
-        List<WebElement> elements = wd.findElements(By.className("entry"));
+        List<WebElement> elements = wd.findElements(By.cssSelector("tr[name='entry']"));
         for (WebElement element : elements) {
-            String firstname = element.findElement(By.xpath("//td[2]")).getText();
-            String lastname =  element.findElement(By.xpath("//td[3]")).getText();
+            String firstname = element.findElement(By.xpath(".//td[3]")).getText();
+            String lastname =  element.findElement(By.xpath(".//td[2]")).getText();
             int id = Integer.parseInt(element.findElement(By.tagName("td")).findElement(By.tagName("input")).getAttribute("value"));
             ContactData contact = new ContactData(id,firstname, null, lastname, null, null, null, null, null,null,null,null,null);
             contacts.add(contact);
