3c2146c1613549138ea755228298294a0e518f41
Зенкова Ирина <i.a.zenkova@novotelecom.ru>
2018-03-02 14:22:40 +0700

    сравнение списков изменено на стравнение множеств по id в классах с группами

--- addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/tests/GroupCreationTests.java
@@ -4,49 +4,19 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.novotelecom.java_training.addressbook.model.GroupData;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
         app.goTo().groupPage();
-        List <GroupData> before = app.group().list();
+        Set <GroupData> before = app.group().all();
         GroupData group = new GroupData().withName("test2");
         app.group().create(group);
-        List <GroupData> after = app.group().list();
-       // Assert.assertEquals(after.size(),before.size()+1);
-
-        //по старинке
-       /* int max=0;
-        for (GroupData g:after) {
-            if (g.getId()>max) {
-                max=g.getId();
-            }
-        }*/
-
-        // чуть более читаемый вид, для понимания того, что тут делалось
-       /* Comparator<? super GroupData> byId = new Comparator<GroupData>() {
-            @Override
-            public int compare(GroupData o1, GroupData o2) {
-                return Integer.compare(o1.getId(),o2.getId());
-            }
-        };*/
-
-        // а вот так можно сделать, начиная с java8
-
-/*      Comparator<? super GroupData> byId = (o1, o2) -> Integer.compare(o1.getId(),o2.getId());
-        int max =  after.stream().max(byId).get().getId();
-        group.setId(max);
-        */
-
-        //group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
+        Set <GroupData> after = app.group().all();
+        group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
         before.add(group);
-
-        Comparator<? super GroupData> byId = (g1,g2) -> Integer.compare(g1.getId(),g2.getId());
-        before.sort(byId);
-        after.sort(byId);
         Assert.assertEquals(before,after);
 
     }
--- addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/tests/GroupDeletionTests.java
@@ -5,14 +5,14 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.novotelecom.java_training.addressbook.model.GroupData;
 
-import java.util.List;
+import java.util.Set;
 
 public class GroupDeletionTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
         app.goTo().groupPage();
-        if (app.group().list().size()==0) {
+        if (app.group().all().size()==0) {
             app.group().create(new GroupData().withName("test1"));
         }
     }
@@ -20,12 +20,11 @@ public class GroupDeletionTests extends TestBase {
     @Test
     public void testGroupDeletion() {
 
-        List<GroupData> before = app.group().list();
-        int index =  before.size() - 1;
-        app.group().delete(index);
-        List<GroupData> after = app.group().list();
-
-        before.remove(index);
+        Set<GroupData> before = app.group().all();
+        GroupData deteledGroup = before.iterator().next();
+        app.group().delete(deteledGroup);
+        Set<GroupData> after = app.group().all();
+        before.remove(deteledGroup);
         Assert.assertEquals(before, after);
     }
 
