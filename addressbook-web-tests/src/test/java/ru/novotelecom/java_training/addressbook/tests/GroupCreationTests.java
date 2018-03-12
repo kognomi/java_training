6286b92931a49db8f17ac15086e09bf01a405a17
Зенкова Ирина <i.a.zenkova@novotelecom.ru>
2018-03-12 15:45:11 +0700

    запуск тестов с данными из сгенеренного файла XML

--- addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,7 @@
 package ru.novotelecom.java_training.addressbook.tests;
 
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.novotelecom.java_training.addressbook.model.GroupData;
@@ -10,6 +11,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,7 +19,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() throws IOException {
+    public Iterator<Object[]> validGroupsFromCsv() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
         BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
         String line = reader.readLine();
@@ -29,7 +31,24 @@ public class GroupCreationTests extends TestBase {
         return list.iterator();
     }
 
-    @Test (dataProvider = "validGroups")
+    @DataProvider
+    public Iterator<Object[]> validGroupsFromXml() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+        String line = reader.readLine();
+        String xml="";
+        while (line!=null) {
+           xml+=line;
+            line = reader.readLine();
+        }
+        XStream xStream = new XStream();
+        xStream.processAnnotations(GroupData.class);
+        List<GroupData> groups = (List<GroupData>)xStream.fromXML(xml);
+        return groups.stream().map((g)-> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
+
+
+    @Test (dataProvider = "validGroupsFromXml")
     public void testGroupCreation(GroupData group ) {
         app.goTo().groupPage();
         Groups before = app.group().all();
