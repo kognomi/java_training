cf67842e06049353a9d795a147671c4578f4b699
Зенкова Ирина <i.a.zenkova@novotelecom.ru>
2018-03-19 13:03:51 +0700

    group modification with db full

--- addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/tests/GroupDeletionTests.java
@@ -13,8 +13,8 @@ public class GroupDeletionTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
-        app.goTo().groupPage();
-        if (app.group().all().size()==0) {
+        if (app.db().groups().size() == 0) {
+            app.goTo().groupPage();
             app.group().create(new GroupData().withName("test1"));
         }
     }
@@ -22,11 +22,12 @@ public class GroupDeletionTests extends TestBase {
     @Test
     public void testGroupDeletion() {
 
-        Groups before = app.group().all();
+        Groups before = app.db().groups();
         GroupData deteledGroup = before.iterator().next();
+        app.goTo().groupPage();
         app.group().delete(deteledGroup);
         assertEquals(app.group().count(),before.size()-1);
-        Groups after = app.group().all();
+        Groups after = app.db().groups();
         assertThat(after,equalTo(before.without(deteledGroup)));
 
     }
