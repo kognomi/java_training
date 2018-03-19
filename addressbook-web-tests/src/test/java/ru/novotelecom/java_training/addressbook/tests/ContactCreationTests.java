7d622a20b92a70099ca9c08a5e6f0117830d2dc3
Зенкова Ирина <i.a.zenkova@novotelecom.ru>
2018-03-19 13:34:21 +0700

    contact creation with db

--- addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/tests/ContactCreationTests.java
@@ -64,11 +64,11 @@ public class ContactCreationTests extends TestBase {
     @Test(dataProvider = "validContactsFromXml")
     public void testContactCreationSet(ContactData contact) {
         app.goTo().homePage();
-        Contacts before = app.contact().all();
+        Contacts before = app.db().contacts();
         app.goTo().contactCreationPage();
         app.contact().create(contact);
         assertThat(app.contact().count(),equalTo(before.size()+1));
-        Contacts after = app.contact().all();
+        Contacts after = app.db().contacts();
         assertThat(after, equalTo(
                 before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
--- addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/novotelecom/java_training/addressbook/tests/ContactModificationTests.java
@@ -26,12 +26,10 @@ public class ContactModificationTests extends TestBase {
     @Test
     public void testContactModificationSet() {
         Contacts before = app.db().contacts();
-        //Contacts before = app.contact().all();
         ContactData modifiedContact = before.iterator().next();
         ContactData contact = new ContactData().withId(modifiedContact.getId()).withFirstname("1Firstname").withMidname("1Midname").withLastname("1Lastname").withAddress("1Addr").withFirstHomePhone("11111").withMobilePhone("12222").withWorkPhone("13333").withFirstEmail("1email1@aaa.aa").withSecondEmail("1email2@bbb.bb").withThirdEmail("1email3@ccc.cc").withSecondHomePhone("15555").withPhoto(new File("src/test/resources/grass.png"));
         app.contact().modify(contact);
         assertEquals(app.contact().count(),before.size());
-        //Contacts after = app.contact().all();
         Contacts after = app.db().contacts();
         assertThat(after,equalTo(before.without(modifiedContact).withAdded(contact)));
     }
